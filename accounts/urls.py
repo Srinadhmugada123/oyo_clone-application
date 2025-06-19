dc2a2cc283eae58755b94940ab7189071dcf488f
Srinadhmugada123 <srinadhmugada523@gmail.com>
2025-06-19 15:36:16 +0530

    ongoing project

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from accounts import views
+
+urlpatterns = [
+    path('login/', views.login_page, name='login_page'),
+    path('register/', views.register, name='register'),
+    path('send_otp/<email>/', views.send_otp, name='send_otp'),
+    path('verify-otp/<email>/', views.verify_otp, name='verify_otp'),
+    path('register/<token>/', views.verify_email_token, name='verify_email_token')
+]
---
+++ accounts/utils.py
@@ -0,0 +1,40 @@
+import uuid
+from django.core.mail import send_mail
+from django.conf import settings
+
+
+def generateRandomToken():
+    return str(uuid.uuid4())
+
+
+def sendEmailToken(email, token):
+    subject = "Verify your email address"
+    message = f"""Hi please verify your email account by this clicking this link
+    http://127.0.0.1:8000/account/register/{token}
+
+    """
+
+    send_mail(
+       subject,
+       message,
+        settings.EMAIL_HOST_USER,
+        [email],
+        fail_silently=False,
+    )
+
+
+
+def sendOTPtoEmail(email, otp):
+    subject = "OTP for account login"
+    message = f"""Hi, use this OTP to login
+       {otp} 
+ 
+    """
+
+    send_mail(
+       subject,
+       message,
+        settings.EMAIL_HOST_USER,
+        [email],
+        fail_silently=False,
+    )
